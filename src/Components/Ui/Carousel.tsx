2cad470820c3f1a1f8921d3aa58e09af43c5c20c
KarimAldeen <MhmadKarimAldeen@gmail.com>
2024-01-20 00:49:21 +0300

    Done

--- src/Components/Ui/Carousel.tsx
+++ src/Components/Ui/Carousel.tsx
@@ -1,31 +1,83 @@
+// import React from 'react';
+// import { Carousel } from 'antd';
+// import { TbannerData } from '../../Layout/app/Types';
+// import { Link } from 'react-router-dom';
+// import { BaseURL } from '../../api/config';
+// import useImageError from '../../Hooks/useImageError';
+// import { UserImageURL } from '../../Layout/app/Const';
+
+
+
+// const CarouselApp= (data:any) => {
+  
+//   return(
+//     <Carousel autoplay>
+//       {data?.data?.data?.map((item:any, index:number) => (
+//         <div className='banner_Container' key={index}>
+//           <img alt='' className='banner1' src={BaseURL+item.slider_translations[0]?.image || UserImageURL} onError={useImageError}  />
+//           {/* <div className='banner_Info'>
+//             <h1 className='underLineText'>{item.title}</h1>
+//             <h2>
+//               {item.subtitle} <br /> <strong>{item.discount}</strong>
+//             </h2>
+//             <Link to={item.link}>{item?.btn} &#8599; </Link>
+//           </div> */}
+//         </div>
+//       ))}
+//     </Carousel>
+//   );
+  
+// }
+// export default CarouselApp;
+
+
 import React from 'react';
 import { Carousel } from 'antd';
-import { TbannerData } from '../../Layout/app/Types';
 import { Link } from 'react-router-dom';
-import { BaseURL } from '../../api/config';
-import useImageError from '../../Hooks/useImageError';
-import { UserImageURL } from '../../Layout/app/Const';
+const bannerData  = [
+    {
+      imageUrl: '../Coins/coins_home_image.jpg',
+      title: 'NEW ARRIVALS',
+      subtitle: 'SUMMER SALE',
+      discount: '20% OFF',
+      link: '/',
+      btn:"Shop Now"
+    },
+     {
+      imageUrl: '../Coins/coins_home_image.jpg',
+      title: 'NEW ARRIVALS',
+      subtitle: 'SUMMER SALE',
+      discount: '20% OFF',
+      link: '/',
+      btn:"Shop Now"
+    },
+     {
+      imageUrl: '../Coins/coins_home_image.jpg',
+      title: 'NEW ARRIVALS',
+      subtitle: 'SUMMER SALE',
+      discount: '20% OFF',
+      link: '/',
+      btn:"Shop Now"
+    },
+  ]
 
+const CarouselApp = (data:any) => (
 
-
-const CarouselApp= (data:any) => {
   
-  return(
-    <Carousel autoplay>
-      {data?.data?.data?.map((item:any, index:number) => (
-        <div className='banner_Container' key={index}>
-          <img alt='' className='banner1' src={BaseURL+item.slider_translations[0]?.image || UserImageURL} onError={useImageError}  />
-          {/* <div className='banner_Info'>
-            <h1 className='underLineText'>{item.title}</h1>
-            <h2>
-              {item.subtitle} <br /> <strong>{item.discount}</strong>
-            </h2>
-            <Link to={item.link}>{item?.btn} &#8599; </Link>
-          </div> */}
+  <Carousel autoplay>
+    {data?.data?.data.map((item:any, index:number) => (
+      <div className='banner_Container' key={index}>
+        <img alt='' className='banner1' src={item.imageUrl} />
+        <div className='banner_Info'>
+          <h1 className='underLineText'>{item.title}</h1>
+          <h2>
+            {item.subtitle} <br /> <strong>{item.discount}</strong>
+          </h2>
+          <Link to={item.link}>{item?.btn} &#8599; </Link>
         </div>
-      ))}
-    </Carousel>
-  );
-  
-}
+      </div>
+    ))}
+  </Carousel>
+);
+
 export default CarouselApp;
