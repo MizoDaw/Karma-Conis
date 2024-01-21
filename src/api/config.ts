c6d388a293d4e47c14c9fc6f8a290315d4477a6f
KarimAldeen <MhmadKarimAldeen@gmail.com>
2024-01-21 12:46:32 +0300

    Done

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 
 
 // export const  BaseURL = "http://api2.qtrendqa.com/"
-export const  BaseURL = "http://192.168.1.4:8000/"
+// export const  BaseURL = "http://192.168.1.4:8000/"
 
 
-// export const  BaseURL = "https://api2.qtrendqa.com/"
+export const  BaseURL = "https://api2.qtrendqa.com/"
