c726e200995e80f54cf08e6d588893f22916ff36
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2024-01-23 14:01:02 +0300

    edit

--- src/Components/Cart/Details/InputCart.tsx
+++ src/Components/Cart/Details/InputCart.tsx
@@ -11,7 +11,7 @@ interface InputCartProps {
 function InputCart({name , placeholder , type = 'text'}:InputCartProps) {
     const {t} = useTranslation()
   return (
-    <div style={{display:"flex" , flexDirection:"column" , width:"100%"}}>
+    <div style={{display:"flex" , flexDirection:"column" , width:"100%" , marginBottom:20}}>
         <Field as={Input} name={name} placeholder={placeholder ??name}  type={type}/>
         <ErrorMessage name={name}>{(message)=><span style={{color:"red"}}>{t(message)}</span>}</ErrorMessage>
     </div>
