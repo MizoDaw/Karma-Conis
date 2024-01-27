785c177bff8c286da953b01921c56437f54cd191
KarimAldeen <MhmadKarimAldeen@gmail.com>
2024-01-27 17:27:37 +0300

    Done

--- src/ProviderContainer.tsx
+++ src/ProviderContainer.tsx
@@ -3,12 +3,15 @@ import QueryProvider from './lib/ReactQueryProvider'
 import { BrowserRouter } from 'react-router-dom'
 import ToastProvider from './lib/ToastProvider'
 import ReduxT from './lib/ReduxT'
+import { useChangeLanguage } from './Hooks/useChangeLanguage'
 
 type ProviderContainerProps =  {
     children:ReactNode
 
 }
 function ProviderContainer({children}:ProviderContainerProps) {
+    const { currentlanguage, changelanguage } = useChangeLanguage();
+
   return (
     <BrowserRouter basename='/'>
         <ReduxT>
